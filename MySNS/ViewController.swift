1ef22d4bf73283692cfcd2bfeb1172f132438a8f
Sangmin Yeom <sangminyeom@gmail.com>
2022-01-13 15:51:23 +0900

    Initial Commit

---
+++ MySNS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySNS
+//
+//  Created by SANGMIN YEOM on 2022/01/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
