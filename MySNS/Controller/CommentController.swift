86e6d9c64c113e8ce8b15c1ae303b45ec6acdbe1
Sangmin Yeom <sangminyeom@gmail.com>
2022-01-25 15:20:50 +0900

    comment posting

--- MySNS/Controller/CommentController.swift
+++ MySNS/Controller/CommentController.swift
@@ -12,6 +12,8 @@ private let reuseIdentifier = "CommentCell"
 class CommentController: UICollectionViewController {
     
     // MARK: - properties
+    private let post: Post
+    private var comments = [Comment]()
 
     private lazy var commentInputView: CommentInputAccesoryView = {
         let frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
@@ -21,10 +23,20 @@ class CommentController: UICollectionViewController {
     }()
     
     // MARK: - lifecycle
+    init(post:Post)
+    {
+        self.post = post
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
+        fetchComments()
     }
     
     override var inputAccessoryView: UIView? {
@@ -47,6 +59,14 @@ class CommentController: UICollectionViewController {
         self.tabBarController?.tabBar.isHidden = false
     }
     
+    // MARK: - api
+    func fetchComments() {
+        CommentService.fetchComments(forPost: post.postId) { comments in
+            self.comments = comments
+            self.collectionView.reloadData()
+        }
+    }
+    
     // MARK: - helpers
     func configureCollectionView() {
         navigationItem.title = "Comments"
@@ -62,11 +82,12 @@ class CommentController: UICollectionViewController {
 // MARK: - UICollectionViewDataSource
 extension CommentController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return self.comments.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CommentCell
+        cell.viewModal = CommentViewModel(comment: comments[indexPath.row])
 
         return cell
     }
@@ -75,14 +96,26 @@ extension CommentController {
 // MARK: - UICollectionViewDelegateFlowLayout
 extension CommentController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 80)
+        
+        let viewModel = CommentViewModel(comment: comments[indexPath.row])
+        let height = viewModel.size(forWidth: view.frame.width).height + 32
+        return CGSize(width: view.frame.width, height: height)
     }
 }
 
 // MARK: - CommentInputAccesoryViewDelegate
 extension CommentController: CommentInputAccesoryViewDelegate {
     func inputView(_ inputView: CommentInputAccesoryView, wantsToUploadComment comment: String) {
-        print("DEBUG: \(comment)")
-        inputView.clearCommentTextView()
+        
+        guard let tab = self.tabBarController as? MainTabController else { return }
+        guard let user = tab.user else { return }
+        
+        self.showLoader(true)
+        
+        CommentService.uploadComment(comment: comment, postId: post.postId, user: user) { error in
+            self.showLoader(false)
+            
+            inputView.clearCommentTextView()
+        }
     }
 }
--- MySNS/View/CommentCell.swift
+++ MySNS/View/CommentCell.swift
@@ -10,6 +10,12 @@ import UIKit
 class CommentCell: UICollectionViewCell {
     
     // MARK: - properties
+    var viewModal : CommentViewModel? {
+        didSet {
+            configure()
+        }
+    }
+    
     private let profileImageView: UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFill
@@ -18,17 +24,7 @@ class CommentCell: UICollectionViewCell {
         return iv
     }()
     
-    private let commentLabel: UILabel = {
-        let label = UILabel()
-        let attributedString  = NSMutableAttributedString(string: "joker  ",
-                                                          attributes: [.font:UIFont.boldSystemFont(ofSize: 14)])
-        attributedString.append(NSAttributedString(string: "some comment for you...",
-                                                   attributes:[.font:UIFont.systemFont(ofSize: 14)]))
-        
-        label.attributedText = attributedString
-        
-        return label
-    }()
+    private let commentLabel = UILabel()
     
     // MARK: - lifecycles
     
@@ -41,12 +37,22 @@ class CommentCell: UICollectionViewCell {
         profileImageView.layer.cornerRadius = 40 / 2
 
         addSubview(commentLabel)
+        commentLabel.numberOfLines = 0
         commentLabel.centerY(inView: profileImageView,
                              leftAnchor: profileImageView.rightAnchor,
                              paddingLeft: 8)
+        commentLabel.anchor(right: self.rightAnchor, paddingRight: 8)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - helpers
+    func configure() {
+        guard let viewModel = viewModal else { return }
+        
+        profileImageView.sd_setImage(with: viewModel.profileImageUrl, completed: nil)
+        commentLabel.attributedText = viewModel.commentLabelText()
     }
 }
